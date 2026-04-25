339d7f411cf76c11c68b560d1803fb500ae0ff3e
MiniFish <minifish@example.com>
2026-04-25 16:05:08 +0800

    Fix serverActions config for Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverActions: true,
-  },
 };
 
 export default nextConfig;
